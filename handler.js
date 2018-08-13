018b5e9020998a0d88925ce9a12a3b9e05885034
Anurag <anurag.saklani4@gmail.com>
2018-08-13 21:38:39 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,45 +1,54 @@
 'use strict';
 const AWS = require('aws-sdk');
-const sqs = new AWS.SQS();
+
 const s3 = new AWS.S3();
-const s3Url = process.env.S3_BUCKET;
-const sqsUrl = process.env.SQS_URL;
+const sqs = new AWS.SQS();
+
+var params = {
+    QueueUrl: process.env.SqsUrl
+};
 
 module.exports.process = (event, context, callback) => {
-    //Step 1: receive message from SQS
-    var reqParams = {
-        QueueUrl: sqsUrl,
-        ReceiveRequestAttemptId: 'reqAttemptId'
-    };
+    sqs.receiveMessage(params, function(err, data) {
+        if (err) {
+            console.log('Receive Error', err);
+        } else if (data.Messages) {
+            var deleteParams = {
+                QueueUrl: params.QueueUrl,
+                ReceiptHandle: data.Messages[0].ReceiptHandle
+            };
+            var startDateTime = Date.now();
+            console.log(data.Messages, null, 2);
 
-    sqs.receiveMessage(reqParams, function(err, data) {
-        if (err) console.log(err, err.stack);
-        // an error occurred
-        else console.log(data); // successful response
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data.Messages[0])
+            };
+
+            setTimeout(deleteMsg, 30000);
+
+            function deleteMsg() {
+                sqs.deleteMessage(deleteParams, function(err, data) {
+                    if (err) {
+                        console.log(`Oooops error : ${err}`);
+                        callback(err);
+                    } else {
+                        console.log(
+                            `Message deleted: ${
+                                deleteParams.ReceiptHandle
+                            } after ${Math.floor(
+                                (Date.now() - startDateTime) / 1000
+                            )} seconds. Remaining Time : ${context.getRemainingTimeInMillis()}`
+                        );
+                        callback(null, {
+                            statusCode: 200,
+                            body: 'successful deletion'
+                        });
+                    }
+                });
+            }
+        }
     });
-
-    //Step 2: Put in S3
-
-    //Step 3: Delete SQS message
-    // var params = {
-    //     QueueUrl: 'sqsUrl',
-    //     ReceiptHandle: data.ReceiptHandle
-    // };
-    // sqs.deleteMessage(params, function(err, data) {
-    //     if (err) console.log(err, err.stack);
-    //     // an error occurred
-    //     else console.log(data); // successful response
-    // });
-
-    // Return response
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Message read from SQS, deleted from SQS and moved to S3'
-        })
-    };
-
-    callback(null, response);
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
